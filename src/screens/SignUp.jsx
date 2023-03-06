73c34bb2dfa62cdbfca349e831afacf5c4dd168a
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-03-06 12:05:20 +0530

    Update SignUp.jsx

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -6,7 +6,7 @@ const SignUp = () => {
     let navigate = useNavigate()
     const handelSubmit = async(e)=>{
         e.preventDefault();
-        const response = await fetch("http://localhost:5000/api/createuser",{
+        const response = await fetch("http://foodgomernbackend.onrender.com/api/createuser",{
            method:'POST',
            headers:{
             'Content-Type': 'application/json'
