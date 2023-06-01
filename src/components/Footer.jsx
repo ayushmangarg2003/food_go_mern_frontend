bf895b05d64bd902fb3e76ed03d1c03917c7ad49
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-06-01 12:33:55 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 my-4 border-top">
-        <div className="text-muted">Copyright © All Rights Reserved</div>
+      <footer className="footer d-flex flex-wrap justify-content-center align-items-center py-3 border-top">
+        <div className="">Copyright © All Rights Reserved</div>
       </footer>
     </div>
   )
