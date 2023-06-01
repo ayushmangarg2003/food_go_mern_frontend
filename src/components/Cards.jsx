de6acd6dafe869569e63c0f9f9542e1d06a2647e
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-06-01 13:10:27 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -41,7 +41,7 @@ const Cards = (props) => {
                         </select>
                     </div>
                     <hr/>
-                    <div className='flex justify-between'>
+                    <div className='d-flex justify-content-between'>
                         <div>₹{finalPrice}/-</div>
                         <button className="btn btn-info text-black justify-center ms-2" onClick={handleAddToCart}>Add to Cart</button>
                     </div>
