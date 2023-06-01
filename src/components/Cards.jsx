c40861f136124f1543e9b5ee9dc8b4fc0bea3420
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2023-06-01 12:53:30 +0530

    Update Cards.jsx

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -27,25 +27,24 @@ const Cards = (props) => {
                 <div className="card-body">
                     <h5 className="card-title text-center">{props.foodItem.name}</h5>
                     <div className='container w-100 justify-between'>
-                        <select className='text-white m-2 h-100 bg-info rounded' onChange={(e)=>setQty(e.target.value)}>
+                        <select className='text-black m-2 h-100 bg-info rounded' onChange={(e)=>setQty(e.target.value)}>
                             {Array.from(Array(6), (e, i) => {
                                 return (
                                     <option key={i + 1} value={i + 1}>{i + 1}</option>
                                 )
                             })}
                         </select>
-                        <select ref={priceRef} className='m-2 h-100 text-white bg-info rounded' onChange={(e)=>setSize(e.target.value)}>
+                        <select ref={priceRef} className='m-2 h-100 text-black bg-info rounded' onChange={(e)=>setSize(e.target.value)}>
                             {price.map((data)=>{
                                 return <option key={data} value={data}>{data}</option>
                             })}
                         </select>
-                        <div className='d-inline h-100 fs-5'>
-                            ₹{finalPrice}/-
-                        </div>
                     </div>
-                    <hr />
                     <div className='text-center'>
-                        <button className="btn btn-info justify-center ms-2" onClick={handleAddToCart}>Add to Cart</button>
+                        <div className='d-inline h-100 fs-5'>
+                            ₹{finalPrice}
+                        </div>
+                        <button className="btn btn-info text-black justify-center ms-2" onClick={handleAddToCart}>Add to Cart</button>
                     </div>
                 </div>
             </div>
